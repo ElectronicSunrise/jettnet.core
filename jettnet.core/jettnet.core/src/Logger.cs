1e9aca43594a4c131a69b34bd9854ea38c09b095
cxxpxr <60411087+cxxpxr@users.noreply.github.com>
2022-02-06 00:07:23 -0500

    Move logger to core

---
+++ jettnet.core/jettnet.core/src/Logger.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace jettnet.logging
+{
+    public enum LogLevel
+    {
+        Info,
+        Warning,
+        Error
+    }
+
+    public class Logger
+    {
+        private readonly Action<object> _error   = Console.Error.WriteLine;
+        private readonly Action<object> _info    = Console.WriteLine;
+        private readonly Action<object> _warning = Console.WriteLine;
+        
+        private readonly string _prefix;
+
+        public Logger(string prefix = "JETTNET")
+        {
+            _prefix = $"[{prefix}]";
+        }
+
+        public Logger(Action<object> info, Action<object> warning, Action<object> error, string prefix = "JETTNET")
+        {
+            _info    = info;
+            _warning = warning;
+            _error   = error;
+            _prefix  = $"[{prefix}]";
+        }
+
+        public void Log(object msg, LogLevel logLevel = LogLevel.Info)
+        {
+            string message = $"{_prefix} {msg}";
+            
+#if UNITY_64
+            switch (logLevel)
+            {
+                case LogLevel.Info:
+                    UnityEngine.Debug.Log(message);
+                    break;
+                case LogLevel.Warning:
+                    UnityEngine.Debug.LogWarning(message);
+                    break;
+                case LogLevel.Error:
+                    UnityEngine.Debug.LogError(message);
+                    break;
+            }
+            
+            return;
+#else
+            switch (logLevel)
+            {
+                case LogLevel.Info:
+                    _info?.Invoke(message);
+                    break;
+                case LogLevel.Warning:
+                    _warning?.Invoke(message);
+                    break;
+                case LogLevel.Error:
+                    _error?.Invoke(message);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(logLevel), logLevel, null);
+            }
+#endif
+        }
+    }
+}
